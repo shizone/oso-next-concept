589ae05c00da14bcc50cad03cb6f02ab0691d317
razon <shizonera@gmail.com>
2018-06-12 18:55:02 +0900

    use vue.js

---
+++ script/header.js
@@ -0,0 +1,13 @@
+﻿var header = new Vue({
+  el: '#header',
+  data: {
+    showMenu: true,
+    menus: [
+             {text: 'タイムテーブル', url: 'timetable.html'},
+             {text: 'よくあるご質問', url: 'faq.html'},
+             {text: 'スポンサー', url: 'sponsor.html'},
+             {text: '実行委員会', url: 'staff.html'},
+             {text: '過去の開催実績', url: 'past.html'}
+           ]
+  }
+})
---
+++ script/staff.js
@@ -0,0 +1,106 @@
+var staff = new Vue({
+  el: '#staff',
+  data: {
+    staffs: [
+             {
+               name: '吉田 貴文', 
+               roll: '実行委員長', 
+               image: 'printing.png', 
+               twitter: 'https://twitter.com/zephiransas', 
+               facebook: 'https://www.facebook.com/takafumi.yoshida.718', 
+               github: '', 
+               external: 'https://zephiransas.github.io'
+             },
+             {
+               name: '角田 裕樹', 
+               roll: '副実行委員長', 
+               image: 'printing.png', 
+               twitter: 'https://twitter.com/razon', 
+               facebook: 'https://www.facebook.com/shiz0ne', 
+               github: 'https://github.com/shizone', 
+               external: 'https://about.me/shizone'
+             },
+             {
+               name: '芝 世弐', 
+               roll: '事務局長', 
+               image: 'printing.png', 
+               twitter: '', 
+               facebook: '', 
+               github: '', 
+               external: ''
+             },
+             {
+               name: '逸見 誠', 
+               roll: '会計', 
+               image: 'printing.png', 
+               twitter: 'https://twitter.com/mako_wis', 
+               facebook: 'https://www.facebook.com/makoto.henmi', 
+               github: 'https://github.com/makowis', 
+               external: 'https://mako-wis.hatenablog.com/'
+             },
+             {
+               name: '阿部 信介', 
+               roll: '会計', 
+               image: 'printing.png', 
+               twitter: 'https://twitter.com/mao_instantlife', 
+               facebook: 'https://www.facebook.com/mao.instantlife', 
+               github: 'https://github.com/Shinsuke-Abe', 
+               external: ''
+             },
+             {
+               name: '吉田 圭佑', 
+               roll: '', 
+               image: 'printing.png', 
+               twitter: 'https://twitter.com/kasoku_ksk', 
+               facebook: 'https://www.facebook.com/keicyanworld', 
+               github: 'https://github.com/kasoku', 
+               external: ''
+             },
+             {
+               name: '三輪 昭生', 
+               roll: '', 
+               image: 'printing.png', 
+               twitter: '', 
+               facebook: '', 
+               github: '', 
+               external: ''
+             },
+             {
+               name: '山本 和久', 
+               roll: '', 
+               image: 'printing.png', 
+               twitter: '', 
+               facebook: '', 
+               github: '', 
+               external: ''
+             },
+             {
+               name: '須増 亮', 
+               roll: '', 
+               image: 'printing.png', 
+               twitter: '', 
+               facebook: '', 
+               github: '', 
+               external: ''
+             },
+             {
+               name: '早坂 竜二', 
+               roll: '', 
+               image: 'printing.png', 
+               twitter: '', 
+               facebook: '', 
+               github: '', 
+               external: ''
+             },
+             {
+               name: '中本 賢吾', 
+               roll: '', 
+               image: 'printing.png', 
+               twitter: 'https://twitter.com/programjp', 
+               facebook: 'https://facebook.com/kengo.nakamoto', 
+               github: 'https://github.com/nakamoto', 
+               external: 'http://asimatch.com'
+             },
+           ]
+  }
+})
