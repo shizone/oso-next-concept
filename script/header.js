11b1c682e0fde21315fe70bf281623d83c8e97c8
razon <shizonera@gmail.com>
2018-06-14 18:40:17 +0900

    fix menu styles (for responsive)

--- script/header.js
+++ script/header.js
@@ -1,7 +1,6 @@
 ﻿var header = new Vue({
   el: '#header',
   data: {
-    showMenu: true,
     menus: [
              {text: 'タイムテーブル', url: 'timetable.html'},
              {text: 'よくあるご質問', url: 'faq.html'},
@@ -9,5 +8,25 @@
              {text: '実行委員会', url: 'staff.html'},
              {text: '過去の開催実績', url: 'past.html'}
            ]
+  },
+  mounted: function () {
+    this.$nextTick(function () {
+      $(window).resize(resize);
+      resize();
+    })
   }
 })
+
+function resize() {
+  var win = $(window).width();
+  var p = 765
+  if(win > p){
+    $("#menus").show();
+  } else {
+    $("#menus").hide();
+  }
+}
+
+function toggleCanvas(){
+  $("#menus").slideToggle();
+}
