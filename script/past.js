49e17be3da1e45737faf02e5611f8ea6688b4af9
razon <shizonera@gmail.com>
2018-09-04 16:15:49 +0900

    add past

---
+++ script/past.js
@@ -0,0 +1,45 @@
+var staff = new Vue({
+  el: '#past',
+  data: {
+    histories: [
+             {
+               year: 2017, 
+               theme: 'レガシーと戦え', 
+               image: '../2017/images/image_1.jpg'
+             },
+             {
+              year: 2016, 
+              theme: 'スマホ祭り', 
+              image: '../2016/images/image_1.jpg'
+            },
+             {
+              year: 2015, 
+              theme: 'IT業界におけるセルフブランディング', 
+              image: '../2015/images/main_1.jpg'
+            },
+             {
+              year: 2014, 
+              theme: 'フロントエンドとサーバーサイド', 
+              image: '../2014/images/main_1.jpg'
+            },
+             {
+              year: 2013
+            },
+             {
+              year: 2012
+            },
+             {
+              year: 2011
+            },
+             {
+              year: 2010
+            },
+            {
+              year: 2009
+            },
+            {
+              year: 2008
+            }
+           ]
+  }
+})
