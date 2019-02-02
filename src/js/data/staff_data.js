e9be579040332975030018c34925ca50208e3125
razon <shizonera@gmail.com>
2019-02-02 14:19:56 +0900

    [WIP]Update Staff

--- src/js/data/staff_data.js
+++ src/js/data/staff_data.js
@@ -1,22 +1,22 @@
 export default {
   staffs: [
     {
-      name: '吉田 貴文',
+      name: '阿部 信介',
       roll: '実行委員長',
       image: 'printing.png',
-      twitter: 'https://twitter.com/zephiransas',
-      facebook: 'https://www.facebook.com/takafumi.yoshida.718',
-      github: '',
-      external: 'https://zephiransas.github.io'
+      twitter: 'https://twitter.com/mao_instantlife',
+      facebook: 'https://www.facebook.com/mao.instantlife',
+      github: 'https://github.com/Shinsuke-Abe',
+      external: ''
     },
     {
-      name: '角田 裕樹',
+      name: '小西 雅也',
       roll: '副実行委員長',
-      image: 'printing.png',
-      twitter: 'https://twitter.com/razon',
-      facebook: 'https://www.facebook.com/shiz0ne',
-      github: 'https://github.com/shizone',
-      external: 'https://about.me/shizone'
+      image: 'konishi.jpg',
+      twitter: 'https://twitter.com/ore_public',
+      facebook: 'https://www.facebook.com/masaya.konishi.92',
+      github: 'https://github.com/ore-public',
+      external: ''
     },
     {
       name: '芝 世弐',
@@ -30,28 +30,46 @@ export default {
     {
       name: '逸見 誠',
       roll: '会計',
-      image: 'printing.png',
+      image: 'henmi.jpg',
       twitter: 'https://twitter.com/mako_wis',
       facebook: 'https://www.facebook.com/makoto.henmi',
       github: 'https://github.com/makowis',
       external: 'https://mako-wis.hatenablog.com/'
     },
     {
-      name: '阿部 信介',
-      roll: '会計',
-      image: 'printing.png',
-      twitter: 'https://twitter.com/mao_instantlife',
-      facebook: 'https://www.facebook.com/mao.instantlife',
-      github: 'https://github.com/Shinsuke-Abe',
-      external: ''
+      name: '角田 裕樹',
+      roll: '',
+      image: 'sumida.png',
+      twitter: 'https://twitter.com/razon',
+      facebook: 'https://www.facebook.com/shiz0ne',
+      github: 'https://github.com/shizone',
+      external: 'https://about.me/shizone'
     },
     {
-      name: '吉田 圭佑',
+      name: '中本 賢吾',
       roll: '',
-      image: 'printing.png',
-      twitter: 'https://twitter.com/kasoku_ksk',
-      facebook: 'https://www.facebook.com/keicyanworld',
-      github: 'https://github.com/kasoku',
+      image: 'nakamoto.jpg',
+      twitter: 'https://twitter.com/programjp',
+      facebook: 'https://facebook.com/kengo.nakamoto',
+      github: 'https://github.com/nakamoto',
+      external: 'http://asimatch.com'
+    },
+    {
+      name: '早坂 竜二',
+      roll: '',
+      image: 'hayasaka.jpg',
+      twitter: 'https://twitter.com/r_hayakasa',
+      facebook: 'https://www.facebook.com/hayakasa',
+      github: 'https://github.com/hayakasa',
+      external: 'https://about.me/ryoji_hayasaka'
+    },
+    {
+      name: '広岡 健太郎',
+      roll: '',
+      image: 'hirooka.jpg',
+      twitter: 'https://twitter.com/LeGenD_BeaT',
+      facebook: 'https://www.facebook.com/kenntarou.hirooka',
+      github: '',
       external: ''
     },
     {
@@ -64,40 +82,22 @@ export default {
       external: ''
     },
     {
-      name: '山本 和久',
+      name: '吉田 圭佑',
       roll: '',
       image: 'printing.png',
-      twitter: '',
-      facebook: '',
-      github: '',
+      twitter: 'https://twitter.com/kasoku_ksk',
+      facebook: 'https://www.facebook.com/keicyanworld',
+      github: 'https://github.com/kasoku',
       external: ''
     },
     {
-      name: '須増 亮',
+      name: '吉田 貴文',
       roll: '',
       image: 'printing.png',
-      twitter: '',
-      facebook: '',
+      twitter: 'https://twitter.com/zephiransas',
+      facebook: 'https://www.facebook.com/takafumi.yoshida.718',
       github: '',
-      external: ''
-    },
-    {
-      name: '早坂 竜二',
-      roll: '',
-      image: 'printing.png',
-      twitter: '',
-      facebook: '',
-      github: '',
-      external: ''
-    },
-    {
-      name: '中本 賢吾',
-      roll: '',
-      image: 'printing.png',
-      twitter: 'https://twitter.com/programjp',
-      facebook: 'https://facebook.com/kengo.nakamoto',
-      github: 'https://github.com/nakamoto',
-      external: 'http://asimatch.com'
+      external: 'https://zephiransas.github.io'
     }
   ]
 }
